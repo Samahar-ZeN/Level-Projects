410f0fdfe7727f802367cc07cc12fe7275b43034
Samaharzen <samaharrajputbhatti67913@gmail.com>
2026-01-15 18:47:43 +0500

    Delete BlogWebsiteProject2/BlogWebsite directory

--- BlogWebsiteProject2/BlogWebsite/Blogs/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from Blogs.models import *
-
-# Register your models here.
-admin.site.register(contactuspage)
-admin.site.register(blogspost)
-admin.site.register(profile)
