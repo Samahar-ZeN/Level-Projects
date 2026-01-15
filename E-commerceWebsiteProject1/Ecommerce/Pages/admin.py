affd1d723a3cf6b4a6625d1e19de777cefea5c5b
Samaharzen <samaharrajputbhatti67913@gmail.com>
2026-01-15 17:00:28 +0500

    E-commerce Website Project

---
+++ E-commerceWebsiteProject1/Ecommerce/Pages/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from Pages.models import*
+
+# Register your models here.
+admin.site.register(ContactusPage)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
